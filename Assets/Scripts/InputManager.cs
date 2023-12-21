555d96dd6a87725fd70e5498821e87f64de69529
TheGamingKn1ght <145138445+TheGamingKn1ght@users.noreply.github.com>
2023-12-21 15:51:02 -0500

    InputManager/SceneStart

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+
+        }
+        
+
+
+
+
+
+
+    }
+}
