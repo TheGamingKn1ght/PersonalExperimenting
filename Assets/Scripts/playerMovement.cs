512482efac97b04b04d99c1ed1df76553288f602
TheGamingKn1ght <145138445+TheGamingKn1ght@users.noreply.github.com>
2024-01-08 15:14:10 -0500

    CollideAndSlide

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,6 +6,7 @@ public class playerMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
     [SerializeField] private Transform groundCheck;
+    [SerializeField] private CapsuleCollider playerCollider;
     [SerializeField] private float jumpHeight = 3f;
     private float height;
     private Vector3 scale; 
@@ -25,10 +26,22 @@ public class playerMovement : MonoBehaviour
     public float gravity = -9.81f;
     Vector3 velocity;
 
+    //Colliding variables
+    int maxBounces = 5;
+    float skinWidth = 0.015f; //inside collider to start collision check
+    private Bounds bounds;
+    float maxSlopeAngle = 55;
+
     private void Start()
     {
         height = controller.height;
         scale = this.transform.localScale;
+    }
+
+    public void directionalInput()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
     }
     // Update is called once per frame
     void Update()
@@ -41,8 +54,12 @@ public class playerMovement : MonoBehaviour
             velocity.y = -2f;
         }
 
+        bounds = playerCollider.bounds;
+        bounds.Expand(-2 * skinWidth);
+
         //Control player movement
         Vector3 move = (transform.right * x) + (transform.forward * z);
+        Vector3 Collision = CollideAndSlide(move, transform.position, 0, move);
         controller.Move(move * speed * Time.deltaTime);
 
         /*
@@ -66,10 +83,59 @@ public class playerMovement : MonoBehaviour
         controller.Move(velocity * Time.deltaTime);
     }
 
-    public void directionalInput()
+    private Vector3 CollideAndSlide(Vector3 vel, Vector3 startPos, int depth, Vector3 initVel) //depth is used to avoid a stack overflow and is compared to maxBounces
     {
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
+        if(depth >= maxBounces)
+        {
+            return Vector3.zero;
+        }
+
+        float dist = vel.magnitude + skinWidth;
+
+        RaycastHit hit;
+        if(Physics.SphereCast(startPos, bounds.extents.x, vel.normalized, out hit, dist, groundMask))
+        {
+            Vector3 snapToSurface = vel.normalized * (hit.distance - skinWidth);
+            Vector3 leftover = vel - snapToSurface;
+            float angle = Vector3.Angle(Vector3.up, hit.normal);
+
+            if(snapToSurface.magnitude <= skinWidth)
+            {
+                snapToSurface = Vector3.zero;
+            }
+            if (angle <= maxSlopeAngle)
+            {
+                leftover = ProjectAndScale(leftover, hit.normal);
+            }
+            //wall or steep slope
+            else
+            {
+                //scale is the value that the velocity will be scaled accoridng to along the wall
+                float scale = 1 - Vector3.Dot(new Vector3(hit.normal.x, 0, hit.normal.z).normalized, -new Vector3(initVel.x, 0, initVel.z).normalized);
+
+                //to avoid jittering at the bottom of a slope
+                //Treats it as a flat wall due to being grounded
+                if (isGrounded)
+                {
+                    leftover = ProjectAndScale(new Vector3(leftover.x, 0, leftover.z), new Vector3(hit.normal.x, 0, hit.normal.z).normalized);
+                    leftover *= scale;
+                }
+                else { leftover = ProjectAndScale(leftover, hit.normal) * scale; }
+            }
+
+            return snapToSurface + CollideAndSlide(leftover, Vector3.positiveInfinity + snapToSurface, depth + 1, initVel);
+        }
+
+        return vel;
+    }
+    
+    private Vector3 ProjectAndScale(Vector3 vec, Vector3 normal)
+    {
+        //Project leftover Vector along the surface and scale it
+        float mag = vec.magnitude;
+        vec = Vector3.ProjectOnPlane(vec, normal).normalized;
+        vec *= mag;
+        return vec;
     }
 
     public void jump()
@@ -100,4 +166,5 @@ public class playerMovement : MonoBehaviour
         speed *= 4f;
         airSpeed *= 4f;
     }
+
 }
